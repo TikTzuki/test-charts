0e6275fcd38534f47b9d86b3414596f1ab0ad2e9
tiktzuki <tranphanthanhlong18@gmail.com>
2023-12-03 15:30:15 +0700

    🐧

--- charts/fab/scripts/update-pull-secret.sh
+++
@@ -1,6 +0,0 @@
-kubectl delete secret/ci-secret -n $NAME_SPACE
-kubectl create secret docker-registry ci-secret \
-  --docker-server=$ECR_REGISTRY \
-  --docker-username=AWS \
-  --docker-password=$(aws ecr --profile default --region $AWS_DEFAULT_REGION get-login-password) \
-  --namespace=$NAME_SPACE
